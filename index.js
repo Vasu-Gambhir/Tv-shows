5896aae41d0e115d5f3d91fc9274bbb481f45262
Vasu Gambhir <vasugambhir15@gmail.com>
2022-08-02 01:08:37 +0530

    search TV shows using the keywords

---
+++ index.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("#searchForm");
+
+
+form.addEventListener("submit", async function(e){
+    e.preventDefault();
+    const searchTerm = form.elements.query.value;
+    const config = {params: {q : searchTerm}}
+    const res = await axios.get(`https://api.tvmaze.com/search/shows/`,config)
+    displayImages(res.data)
+    form.elements.query.value = ""
+})
+
+const displayImages = (shows) => {
+    for(let result of shows){
+        if(result.show.image){
+            const newImg = document.createElement("IMG");
+            newImg.src = result.show.image.medium
+            document.body.append(newImg);
+        }
+    }
+}
